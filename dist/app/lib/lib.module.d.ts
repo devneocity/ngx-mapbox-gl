15ef6db1f86966a6dc336d209ffe5cc4bc079905
Charles CORCY <ccorcy@student.42.fr>
2019-10-02 15:59:13 +0200

    build with aot

--- dist/app/lib/lib.module.d.ts
+++
@@ -1,7 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export declare class NgxMapboxGLModule {
-    static forRoot(config: {
-        accessToken: string;
-        geocoderAccessToken?: string;
-    }): ModuleWithProviders;
-}
